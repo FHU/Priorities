acc0c041f2dfe87762de7ab5be6e167c2a4f63d2
LTsoccer <77810494+LTsoccer@users.noreply.github.com>
2023-10-24 13:45:44 -0500

    adding basic layout

--- Priorities/ViewModels/RoundResultsPageViewModel.cs
+++ Priorities/ViewModels/RoundResultsPageViewModel.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Priorities.Models;
 
 namespace Priorities.ViewModels
 {
-    internal class RoundResultsPageViewModel
+    public class RoundResultsPageViewModel : ObservableObject
     {
+        public RoundResultsPageViewModel()
+        {
+        }
     }
+
+    
 }
