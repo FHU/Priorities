90e8a7635a360f9528370afe8f82889bc61cc95a
Kaitlyn Thompson <89215697+PenguinEmpress@users.noreply.github.com>
2023-10-25 09:42:57 -0500

    Fixing Errors

--- Priorities/Views/HighScorePage.xaml.cs
+++ Priorities/Views/HighScorePage.xaml.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Priorities
+﻿namespace Priorities.Views
 {
-    internal class HighScorePage
+    public partial class HighScorePage : ContentPage
     {
+        public HighScorePage()
+        {
+            InitializeComponent();
+        }
     }
 }
