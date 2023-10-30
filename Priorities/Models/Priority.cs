06550eb47969b88002fecc63a09be31ae9534052
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2023-10-30 15:15:50 -0500

    Bind Priority Strings to View - 10/30

---
+++ Priorities/Models/Priority.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Priorities.Models
+{
+	public class Priority
+	{
+		public string PriorityOne { get; set; }
+		public string PriorityTwo { get; set; }
+		public string PriorityThree { get; set; }
+		public string PriorityFour { get; set; }
+		public string PriorityFive { get; set; }
+
+		public Priority()
+		{
+		}
+	}
+}
+
--- Priorities/ViewModels/GamePageViewModel.cs
+++ Priorities/ViewModels/GamePageViewModel.cs
@@ -5,13 +5,36 @@ namespace Priorities.ViewModels
 {
     public partial class GamePageViewModel : ObservableObject
     {
+        /*Player Properties (Abbie Vance)*/
         [ObservableProperty]
         public string playerName;
 
         [ObservableProperty]
         public string playerImage;
-        public GamePageViewModel() 
+
+        /*Priority Card Properties (Priscilla Harris)*/
+        [ObservableProperty]
+        public string thingOne;
+
+        [ObservableProperty]
+        public string thingTwo;
+
+        [ObservableProperty]
+        public string thingThree;
+
+        [ObservableProperty]
+        public string thingFour;
+
+        [ObservableProperty]
+        public string thingFive;
+
+        /*Round and Score Properties (Gavin Boler)*/
+
+        /*Timer (Luke Noles)*/
+
+        public GamePageViewModel()
         {
+            /*Abbie*/
             var playerOne = new Player
             {
                 Name = "Gavin Boler",
@@ -20,6 +43,42 @@ namespace Priorities.ViewModels
 
             playerName = playerOne.Name;
             playerImage = playerOne.ImageName;
+
+            /*Priscilla*/
+            var priorityOne = new Priority
+            {
+                PriorityOne = "Turtles"
+            };
+
+            var priorityTwo = new Priority
+            {
+                PriorityTwo = "Evan Kahan"
+            };
+
+            var priorityThree = new Priority
+            {
+                PriorityThree = "Microwaves"
+            };
+
+            var priorityFour = new Priority
+            {
+                PriorityFour = "Musicals"
+            };
+
+            var priorityFive = new Priority
+            {
+                PriorityFive = "Calculus"
+            };
+
+            thingOne = priorityOne.PriorityOne;
+            thingTwo = priorityTwo.PriorityTwo;
+            thingThree = priorityThree.PriorityThree;
+            thingFour = priorityFour.PriorityFour;
+            thingFive = priorityFive.PriorityFive;
+
+            /*Gavin*/
+
+            /*Luke*/
 
         }
     }
