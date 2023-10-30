f25bc02edfa3283a91628b597c68d82acf98356d
LTsoccer <77810494+LTsoccer@users.noreply.github.com>
2023-10-30 12:12:53 -0500

    added variables

--- Priorities/Models/Ranking.cs
+++ Priorities/Models/Ranking.cs
@@ -7,6 +7,10 @@ namespace Priorities.Models
         public int Number { get; set; }
         public string Name { get; set; }
 
+        public string ImagePath { get; set; }
+
+        public int Points { get; set; }
+
         public Ranking()
         {
         }
