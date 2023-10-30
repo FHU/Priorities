ec692e05c2ca72c8afa63e0fed40761b0c800589
Kenan Casey <kenancasey@gmail.com>
2023-10-30 14:22:38 -0500

    Add basic data binding

---
+++ Priorities/ViewModels/AchievementsPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Priorities.Models;
+
+namespace Priorities.ViewModels
+{
+	public class AchievementsPageViewModel : ObservableObject
+	{
+
+		public ObservableCollection<Achievement> Achievements { get; set; }
+
+
+		public AchievementsPageViewModel()
+		{
+
+			Achievements = new ObservableCollection<Achievement>();
+
+			var achievement = new Achievement()
+			{
+				Title = "Snail's Pace",
+				Description = "Finish voting within 5 secs of timer deadline.",
+				Unlocked = true
+            };
+
+			Achievements.Add(achievement);
+
+			achievement = new Achievement()
+			{
+				Title = "Fastest Man Alive",
+				Description = "Prioritizer finishes his decision within 30 secs.",
+				Unlocked = false
+			};
+
+            Achievements.Add(achievement);
+
+        }
+    }
+}
+
---
+++ Priorities/Views/AchievementsPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Priorities.ViewModels;
+
+namespace Priorities.Views;
+
+public partial class AchievementsPage : ContentPage
+{
+	public AchievementsPage()
+	{
+		InitializeComponent();
+
+		BindingContext = new AchievementsPageViewModel();
+	}
+}
