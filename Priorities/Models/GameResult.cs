40e106f47f9def46407f2ce1afe8ec23920c33ed
marvelkel17 <kelly.sproule@students.fhu.edu>
2023-11-02 22:08:01 -0500

    Update ViewModel

--- Priorities/Models/GameResult.cs
+++ Priorities/Models/GameResult.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using Priorities.Models;
+
 namespace Priorities.Models
 {
 	public class GameResult
 	{
 		public Team Team { get; set; }
 		public int Score { get; set; }
-
 		public DateTime Date { get; set; }
-
+		public ObservableCollection<Achievement> Achievements { get; set; }
 		public GameResult()
 		{
 		}
---
+++ Priorities/ViewModels/HighScorePageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Priorities.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priorities.ViewModels
+{
+    public class HighScorePageViewModel : ObservableObject
+    {
+        public ObservableCollection<GameResult> GameResults { get; set; }
+        public HighScorePageViewModel()
+        {
+            GameResults = new ObservableCollection<GameResult>();
+
+        }
+    }
+}
