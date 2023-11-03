54805a2fd63c3fed8a217b2c26fdf2b9156c766e
sflowers51 <samuel.flowers@students.fhu.edu>
2023-11-03 08:46:32 -0500

    functions from the viewmodel assignment put in.

--- Priorities/ViewModels/PlayersPageViewModel.cs
+++ Priorities/ViewModels/PlayersPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Reflection;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Priorities.Models;
 
@@ -14,7 +15,48 @@ namespace Priorities.ViewModels
 		public PlayersPageViewModel()
 		{
             Players = new ObservableCollection<Player>();
+		}
+
+			//Model
+			//Player
+
+			//string Name
+
+			//string ImagePath
+
+			//View Model
+			//Properties
+			//ObservableCollection<Player> Players
+
+			//Commands
+			//AddPlayer()
+
+			//RemovePlayer(Player)
+
+			//EditPlayer(Player)
+
+			//PlayGame()
+
+		public void AddPlayers()
+		{
+            throw new NotImplementedException();
         }
-	}
+
+		public void RemovePlayers()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void EditPlayer()
+		{
+			throw new NotImplementedException();
+		} 
+
+		public void PlayeGame()
+		{
+			throw new NotImplementedException();
+		}
+
+    }
 }
 
