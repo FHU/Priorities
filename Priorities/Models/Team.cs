35ff1b3533261cfd00a9cd09e69d395254d3a194
marvelkel17 <kelly.sproule@students.fhu.edu>
2023-11-08 13:46:38 -0600

    Add CollectionViews
    Working on dynamic grids to see achievements and players

--- Priorities/Models/Team.cs
+++ Priorities/Models/Team.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Priorities.Views;
+using System;
+using System.Collections.ObjectModel;
+
 namespace Priorities.Models
 {
 	public class Team
 	{
 		public string Name { get; set; }
 
-		List<Player> Players { get; set; }
+		public ObservableCollection<Player> Players { get; set; }
 
 
 		public Team()
--- Priorities/ViewModels/HighScorePageViewModel.cs
+++ Priorities/ViewModels/HighScorePageViewModel.cs
@@ -12,24 +12,33 @@ namespace Priorities.ViewModels
     public partial class HighScorePageViewModel : ObservableObject
     {
         public ObservableCollection<GameResult> GameResults { get; set; }
-        public ObservableCollection<Achievement> achievements { get; private set; }
+
+        public ObservableCollection<Achievement> Achievements { get; set; }
 
         public HighScorePageViewModel()
         {
             GameResults = new ObservableCollection<GameResult>();
 
+
             GameResult = new GameResult()
             {
                 Score = 4999,
                 Date = DateTime.Now,
-                Team = new Team() { Name="Skellingtons"},
-                Achievements = achievements
+                Team = new Team() { Name = "Skellingtons" },
+                Achievements = new ObservableCollection<Achievement>
+                {
+                            new Achievement() { Title="Title 1", Unlocked=true, DateUnlocked = DateTime.Now , Description = "blah"},
+                            new Achievement() { Title="Title 2", Unlocked=true, DateUnlocked = DateTime.Now, Description="no" }, 
+                            new Achievement() { Title="Title 2", Unlocked=true, DateUnlocked = DateTime.Now, Description="double no" }
+  },
             };
             GameResults.Add(GameResult);
-            
 
         }
         [ObservableProperty]
         private GameResult gameResult;
+
+        [ObservableProperty]
+        private Achievement gameAchievement;
     }
 }
