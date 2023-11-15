4b30bd6210c5c8c5305db9b6567c72ed1fe1532a
tuckerbrown5 <tucker.brown@students.fhu.edu>
2023-11-15 09:45:22 -0600

    getReadyPageFinal

--- Priorities/ViewModels/GetReadyPageViewModel.cs
+++ Priorities/ViewModels/GetReadyPageViewModel.cs
@@ -1,11 +1,34 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Priorities.Models;
+
 namespace Priorities.ViewModels
 {
-	public class GetReadyPageViewModel
-	{
-		public GetReadyPageViewModel()
-		{
-		}
-	}
-}
+    public partial class GetReadyPageViewModel : ObservableObject
+    {
 
+        [ObservableProperty]
+        Player currentPlayer;
+
+        public ObservableCollection<Player> Players { get; set; }
+
+
+
+
+        public GetReadyPageViewModel()
+        {
+            Players = new ObservableCollection<Player>
+            {
+                new Player{Name="Kenan", ImageName="kenan.jpg"},
+                new Player{Name="David", ImageName="shannon.jpg"},
+                new Player{Name="TJ", ImageName="tj.jpg"}
+            };
+
+            var playerCurrent = Players[1];
+            CurrentPlayer = playerCurrent;
+
+
+        }
+    }
+}
