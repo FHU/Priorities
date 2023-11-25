965012a8d5dd44a4bc2892c45e3b67f48da0ca14
AveryH6 <89232897+AveryH6@users.noreply.github.com>
2023-11-25 12:24:23 -0600

    take/retake pic

--- Priorities/Views/AddPlayerPage.xaml.cs
+++ Priorities/Views/AddPlayerPage.xaml.cs
@@ -22,6 +22,10 @@ public partial class AddPlayerPage : ContentPage
 
         if( cameraView.Cameras.Count > 0)
         {
+            // Use this for real cameras
+            // cameraView.Camera = cameraView.Cameras[1];
+
+            // Use this for simulators
             cameraView.Camera = cameraView.Cameras.First();
 
             MainThread.BeginInvokeOnMainThread(async () =>
@@ -35,11 +39,28 @@ public partial class AddPlayerPage : ContentPage
             Console.WriteLine("No CAMERAS");
         }
 
+        
+
        
     }
 
     void Button_Clicked(System.Object sender, System.EventArgs e)
     {
         avatarPic.Source = cameraView.GetSnapShot(Camera.MAUI.ImageFormat.JPEG);
+        cameraView.StopCameraAsync();
+        avatarPic.IsVisible = true;
+        cameraView.IsVisible = false;
+        smile.IsVisible = false;
+        retake.IsVisible = true;
+
+    }
+
+    void Button_Clicked_1(System.Object sender, System.EventArgs e)
+    {
+        cameraView.StartCameraAsync();
+        avatarPic.IsVisible = false;
+        cameraView.IsVisible = true;
+        smile.IsVisible = true;
+        retake.IsVisible = false;
     }
 }
