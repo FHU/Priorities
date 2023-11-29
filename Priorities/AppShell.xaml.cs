60ca6004debd8919acc20a7886d22d0340c530fc
sflowers51 <111446369+sflowers51@users.noreply.github.com>
2023-11-29 14:00:27 -0600

    routes part 1

--- Priorities/AppShell.xaml.cs
+++ Priorities/AppShell.xaml.cs
@@ -1,10 +1,21 @@
 ﻿namespace Priorities;
-
+using Views;
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
-	}
+
+
+		Routing.RegisterRoute(nameof(AddPlayerPage), typeof(AddPlayerPage));
+        Routing.RegisterRoute(nameof(Views.GamePage), typeof(Views.GamePage));
+        Routing.RegisterRoute(nameof(Views.GameResultsPage), typeof(Views.GameResultsPage));
+        Routing.RegisterRoute(nameof(Views.GameStatusBar), typeof(Views.GameStatusBar));
+        Routing.RegisterRoute(nameof(Views.GetReadyPage), typeof(Views.GetReadyPage));
+        Routing.RegisterRoute(nameof(Views.HighScorePage), typeof(Views.HighScorePage));
+        Routing.RegisterRoute(nameof(Views.HomePage), typeof(Views.HomePage));
+        Routing.RegisterRoute(nameof(Views.PlayersPage), typeof(Views.PlayersPage));
+        Routing.RegisterRoute(nameof(Views.RoundResultsPage), typeof(Views.RoundResultsPage));
+    }
 }
 
