efdbf88ea1034c3636020c616e7e9b5aec8e97b0
kayleeodom <kaylee.odom@students.fhu.edu>
2023-12-04 09:40:16 -0600

    char limit

--- Priorities/Views/AddPlayerPage.xaml.cs
+++ Priorities/Views/AddPlayerPage.xaml.cs
@@ -1,6 +1,6 @@
 namespace Priorities.Views;
 
-using Camera.MAUI;
+//using Camera.MAUI;
 using Priorities.Services;
 using Priorities.ViewModels;
 
@@ -18,50 +18,61 @@ public partial class AddPlayerPage : ContentPage
         Shell.Current.FlyoutIsPresented = true;
     }
 
-    void cameraView_CamerasLoaded(System.Object sender, System.EventArgs e)
+    private void Entry_TextChanged(object sender, TextChangedEventArgs e)
     {
+        Entry entry = (Entry)sender;
 
-        if (cameraView.Cameras.Count > 0)
-        {
-            // Use this for real cameras
-            // cameraView.Camera = cameraView.Cameras[1];
+        int currentLength = entry.Text.Length;
+        int maxLength = entry.MaxLength;
 
-            // Use this for simulators
-            cameraView.Camera = cameraView.Cameras.First();
-
-            MainThread.BeginInvokeOnMainThread(async () =>
-            {
-                await cameraView.StopCameraAsync();
-                var result = await cameraView.StartCameraAsync();
-            });
-        }
-        else
-        {
-            Console.WriteLine("No CAMERAS");
-        }
-
-        
-
-       
+        // Update the character count label 	
+        charLimitLabel.Text = $"{currentLength}/{maxLength}";
     }
 
-    void Button_Clicked(System.Object sender, System.EventArgs e)
-    {
-        avatarPic.Source = cameraView.GetSnapShot(Camera.MAUI.ImageFormat.JPEG);
-        cameraView.StopCameraAsync();
-        avatarPic.IsVisible = true;
-        cameraView.IsVisible = false;
-        smile.IsVisible = false;
-        retake.IsVisible = true;
+    //void cameraView_CamerasLoaded(System.Object sender, System.EventArgs e)
+    //{
 
-    }
+    //    if (cameraView.Cameras.Count > 0)
+    //    {
+    //        // Use this for real cameras
+    //        // cameraView.Camera = cameraView.Cameras[1];
 
-    void Button_Clicked_1(System.Object sender, System.EventArgs e)
-    {
-        cameraView.StartCameraAsync();
-        avatarPic.IsVisible = false;
-        cameraView.IsVisible = true;
-        smile.IsVisible = true;
-        retake.IsVisible = false;
-    }
+    //        // Use this for simulators
+    //        cameraView.Camera = cameraView.Cameras.First();
+
+    //        MainThread.BeginInvokeOnMainThread(async () =>
+    //        {
+    //            await cameraView.StopCameraAsync();
+    //            var result = await cameraView.StartCameraAsync();
+    //        });
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("No CAMERAS");
+    //    }
+
+
+
+
+    //}
+
+    //void Button_Clicked(System.Object sender, System.EventArgs e)
+    //{
+    //    avatarPic.Source = cameraView.GetSnapShot(Camera.MAUI.ImageFormat.JPEG);
+    //    cameraView.StopCameraAsync();
+    //    avatarPic.IsVisible = true;
+    //    cameraView.IsVisible = false;
+    //    smile.IsVisible = false;
+    //    retake.IsVisible = true;
+
+    //}
+
+    //void Button_Clicked_1(System.Object sender, System.EventArgs e)
+    //{
+    //    cameraView.StartCameraAsync();
+    //    avatarPic.IsVisible = false;
+    //    cameraView.IsVisible = true;
+    //    smile.IsVisible = true;
+    //    retake.IsVisible = false;
+    //}
 }
