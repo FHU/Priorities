dc5bf0bfb5a7b4d37698337694ac1039f85ab2d3
AveryH6 <89232897+AveryH6@users.noreply.github.com>
2023-12-06 10:18:48 -0600

    remove comments

--- Priorities/Views/AddPlayerPage.xaml.cs
+++ Priorities/Views/AddPlayerPage.xaml.cs
@@ -1,6 +1,5 @@
 namespace Priorities.Views;
 
-//using Camera.MAUI;
 using Priorities.Services;
 using Priorities.ViewModels;
 
@@ -29,50 +28,5 @@ public partial class AddPlayerPage : ContentPage
         charLimitLabel.Text = $"{currentLength}/{maxLength}";	
     }
 
-    //void cameraView_CamerasLoaded(System.Object sender, System.EventArgs e)
-    //{
 
-    //    if (cameraView.Cameras.Count > 0)
-    //    {
-    //        // Use this for real cameras
-    //        // cameraView.Camera = cameraView.Cameras[1];
-
-    //        // Use this for simulators
-    //        cameraView.Camera = cameraView.Cameras.First();
-
-    //        MainThread.BeginInvokeOnMainThread(async () =>
-    //        {
-    //            await cameraView.StopCameraAsync();
-    //            var result = await cameraView.StartCameraAsync();
-    //        });
-    //    }
-    //    else
-    //    {
-    //        Console.WriteLine("No CAMERAS");
-    //    }
-
-        
-
-       
-    //}
-
-    //void Button_Clicked(System.Object sender, System.EventArgs e)
-    //{
-    //    avatarPic.Source = cameraView.GetSnapShot(Camera.MAUI.ImageFormat.JPEG);
-    //    cameraView.StopCameraAsync();
-    //    avatarPic.IsVisible = true;
-    //    cameraView.IsVisible = false;
-    //    smile.IsVisible = false;
-    //    retake.IsVisible = true;
-
-    //}
-
-    //void Button_Clicked_1(System.Object sender, System.EventArgs e)
-    //{
-    //    cameraView.StartCameraAsync();
-    //    avatarPic.IsVisible = false;
-    //    cameraView.IsVisible = true;
-    //    smile.IsVisible = true;
-    //    retake.IsVisible = false;
-    //}
 }
