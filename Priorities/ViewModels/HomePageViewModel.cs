bfbd6929215fcd2253ba9c7aa8f08c8b6997a3a9
allisoncwalker <82103388+allisoncwalker@users.noreply.github.com>
2023-12-06 16:44:21 -0600

    add viewmodel

---
+++ Priorities/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using Priorities.Views;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Priorities.Models;
+using System.Collections.ObjectModel;
+using Priorities.Services;
+
+namespace Priorities.ViewModels
+{
+	public partial class HomePageViewModel
+	{
+		public HomePageViewModel()
+		{
+            [RelayCommand]
+            async Task NavigateToPlayersPage()
+            {
+                await Shell.Current.GoToAsync(nameof(PlayersPage));
+            }
+        }
+	}
+}
+
