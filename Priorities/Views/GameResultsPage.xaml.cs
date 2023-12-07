d67ca70aa251123ad75534ae0f99c1cd86b98026
SeanJaniec <78286187+SeanJaniec@users.noreply.github.com>
2023-12-07 09:43:08 -0600

    Implemented navigation
    navigation for Players page and high score page.

--- Priorities/Views/GameResultsPage.xaml.cs
+++ Priorities/Views/GameResultsPage.xaml.cs
@@ -1,15 +1,17 @@
 ﻿using Priorities.ViewModels;
-
+using Priorities.Services;
 namespace Priorities.Views;
 
 public partial class GameResultsPage : ContentPage
 {
-	public GameResultsPage()
+    private GameResultsPageViewModel viewModel;
+    public GameResultsPage(IGameStateService gameStateService)
 	{
 		InitializeComponent();
 
-		BindingContext = new GameResultsPageViewModel();
-	}
+        viewModel = new GameResultsPageViewModel(gameStateService);
+        BindingContext = viewModel;
+    }
 
     void HamburgerMenuButton_Clicked(System.Object sender, System.EventArgs e)
     {
