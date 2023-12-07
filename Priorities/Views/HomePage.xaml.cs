8194ec6fe73537c6a7c4fb9523ccc690aa5a8a69
savannah-martin <savannahmartin454@gmail.com>
2023-12-06 21:39:47 -0600

    add home page functionality

--- Priorities/Views/HomePage.xaml.cs
+++ Priorities/Views/HomePage.xaml.cs
@@ -1,9 +1,28 @@
-﻿namespace Priorities.Views;
+﻿using Priorities.ViewModels;
+namespace Priorities.Views;
 
 public partial class HomePage : ContentPage
 {
-	public HomePage()
-	{
-		InitializeComponent();
-	}
+    public HomePage()
+    {
+        InitializeComponent();
+
+    }
+
+    async void Start_Clicked(System.Object sender, System.EventArgs e)
+    {
+        await Shell.Current.GoToAsync($"{nameof(PlayersPage)}");
+    }
+    async void High_Scores_Clicked(System.Object sender, System.EventArgs e)
+    {
+        await Shell.Current.GoToAsync($"{nameof(HighScorePage)}");
+    }
+    async void Achievements_Clicked(System.Object sender, System.EventArgs e)
+    {
+        await Shell.Current.GoToAsync($"{nameof(PlayersPage)}");
+    }
+    async void How_To_Play_Clicked(System.Object sender, System.EventArgs e)
+    {
+        await Shell.Current.GoToAsync($"{nameof(PlayersPage)}");
+    }
 }
