acf16f37164ba627bbd4fd6eceb71a91948a9551
savannah-martin <savannahmartin454@gmail.com>
2023-12-08 19:25:12 -0600

    Revert "Merge branch 'main' into gamepage"
    This reverts commit 8a6fe344e9ec89479c012f9f3279610d7465960a, reversing
    changes made to 6af953ca8341b9a3e538443f47e4480d029daadb.

---
+++ Priorities/ViewModels/AddPlayerPageViewModel.cs
@@ -0,0 +1,67 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Priorities.Models;
+using Priorities.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priorities.ViewModels
+{
+    public partial class AddPlayerPageViewModel : ObservableObject
+    {
+
+        private readonly IGameStateService gameStateService;
+
+        /* name entry */
+        [ObservableProperty]
+        private string name;
+
+        /* avatar image */
+        [ObservableProperty]
+        private string image;
+
+
+        public AddPlayerPageViewModel(IGameStateService gameStateService)
+        {
+            this.gameStateService = gameStateService;
+        }
+
+        [RelayCommand]
+        async Task TakePhoto()
+        {
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+
+                if (photo != null)
+                {
+                    // save the file into local storage
+                    string localFilePath = Path.Combine(FileSystem.AppDataDirectory, photo.FileName);
+
+                    using Stream sourceStream = await photo.OpenReadAsync();
+                    using FileStream localFileStream = File.OpenWrite(localFilePath);
+
+                    await sourceStream.CopyToAsync(localFileStream);
+
+                    Image = localFilePath;
+                }
+            }
+        }
+
+        [RelayCommand]
+        void AddPlayer()
+        {
+
+            gameStateService.Players.Add(
+                new Player() { Name = this.Name, ImageName = Image });
+
+            Shell.Current.GoToAsync("../");
+
+        }
+
+    }
+
+}
--- Priorities/ViewModels/GamePageViewModel.cs
+++ Priorities/ViewModels/GamePageViewModel.cs
@@ -68,6 +68,7 @@ namespace Priorities.ViewModels
             playerImage = gameStateService.Prioritizer.ImageName;
 
             /*Priscilla*/
+
             Priorities = new ObservableCollection<Priority>();
 
             if (phase.Equals(GamePhase.Prioritizing))
@@ -91,7 +92,7 @@ namespace Priorities.ViewModels
             }
 
             /*Gavin*/
-            Round = gameStateService.Round;
+
             TotalRounds = gameStateService.TotalRounds;
 
             /*Priscilla*/
@@ -109,13 +110,13 @@ namespace Priorities.ViewModels
                 prioritizing = false;
                 guessing = true;
             }
-            
+
 
         }
 
 
         [RelayCommand]
-        async Task NavigateToRoundResultsPage()
+        async Task NavigateToNextPage()
         {
             var rankingList = new List<string>();
             foreach (var priority in Priorities)
@@ -131,8 +132,8 @@ namespace Priorities.ViewModels
             else
             {
                 gameStateService.GuesserRankings = rankingList;
+                await Shell.Current.GoToAsync(nameof(RoundResultsPage));
             }
-            await Shell.Current.GoToAsync(nameof(RoundResultsPage));
         }
 
         // I got this off the internet
